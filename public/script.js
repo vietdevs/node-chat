f4d1decd4a08ed1714b51fd6c46d292de7d8b909
Nicolas <nicolas.antoine@epitech.eu>
2019-06-10 17:27:59 +0200

    [ADD]: Functionnal message broadcasting

--- public/script.js
+++ public/script.js
@@ -2,16 +2,39 @@ let messages = document.getElementsByClassName('message-list')[0];
 let message_input = document.getElementById('message-input');
 let message_form = document.getElementById('message-form');
 
+let socket = io();
+
 messages.scrollTop = messages.scrollHeight;
 
 message_input.addEventListener('keydown', (event) => {
-    if (!event.shiftKey) {
+    if (event.keyCode == 13 /*enter*/ && !event.shiftKey) {
         event.preventDefault();
-        console.log("Return pressed");
+        sendMessage();
     }
+    return false;
 });
 
 message_form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log("Submitted");
+    sendMessage();
+    return false;
+});
+
+function sendMessage()
+{
+    let new_message = document.createElement('li');
+
+    socket.emit('message', message_input.value);
+    new_message.innerHTML = message_input.value;
+    new_message.className = 'message me';
+    messages.append(new_message);
+    message_input.value = '';
+}
+
+socket.on('message', (message) => {
+    let new_message = document.createElement('li');
+    
+    new_message.innerHTML = message;
+    new_message.className = 'message';
+    messages.append(new_message);
 });
--- src/main.js
+++ src/main.js
@@ -7,8 +7,17 @@ app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('message', (message) => {
+        console.log('message: ' + message);
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
-app.listen(3000, () => {
+http.listen(3000, () => {
     console.log('listening on *:3000');
 });
