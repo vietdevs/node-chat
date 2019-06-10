2ac9acf4a25160fccd622681584822036f0fa548
Nicolas <nicolas.antoine@epitech.eu>
2019-06-10 18:47:26 +0200

    [ADD]: Pre-connection message loading on connection

--- public/script.js
+++ public/script.js
@@ -4,8 +4,6 @@ let message_form = document.getElementById('message-form');
 
 let socket = io();
 
-messages.scrollTop = messages.scrollHeight;
-
 message_input.addEventListener('keydown', (event) => {
     if (event.keyCode == 13 /*enter*/ && !event.shiftKey) {
         event.preventDefault();
@@ -24,11 +22,14 @@ function sendMessage()
 {
     let new_message = document.createElement('li');
 
+    if (message_input.value === '')
+        return false;
     socket.emit('message', message_input.value);
     new_message.innerHTML = message_input.value;
     new_message.className = 'message me';
     messages.append(new_message);
     message_input.value = '';
+    messages.scrollTop = messages.scrollHeight;
 }
 
 socket.on('message', (message) => {
@@ -37,4 +38,25 @@ socket.on('message', (message) => {
     new_message.innerHTML = message;
     new_message.className = 'message';
     messages.append(new_message);
+    messages.scrollTop = messages.scrollHeight;
+});
+
+socket.on('your_ip', (ip) => {
+    fetch('./messages')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        for (let i = 0; i < myJson.length; i += 1) {
+            let current_message = document.createElement('li');
+
+            current_message.innerHTML = myJson[i].message;
+            if (ip === myJson[i].ip)
+                current_message.className = "message me";
+            else
+                current_message.className = "message";
+            messages.append(current_message);
+        }
+        messages.scrollTop = messages.scrollHeight;
+    });
 });
--- src/main.js
+++ src/main.js
@@ -5,17 +5,29 @@ let io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+let messages = [];
+let current_message_id = 0;
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.emit('your_ip', socket.handshake.address);
 
     socket.on('message', (message) => {
-        console.log('message: ' + message);
+        let address = socket.handshake.address;
+
+        console.log('message: ' + message + ' from ' + address);
         socket.broadcast.emit('message', message);
+        messages.push({'ip': address, 'id': current_message_id, 'message': message});
+        current_message_id += 1;
     });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
+});
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
 });
 
 http.listen(3000, () => {
