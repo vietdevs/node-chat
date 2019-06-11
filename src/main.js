0c1a030875f8b7a3f814c545c6e4cf28259d667f
Nicolas <nicolas.antoine@epitech.eu>
2019-06-11 11:59:48 +0200

    [ADD]: unique ip hashing with salt for better pricacy

--- src/main.js
+++ src/main.js
@@ -2,18 +2,20 @@ let express = require('express');
 let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+const bcrypt = require('bcrypt');
 
 app.use(express.static('public'));
 
 let messages = [];
 let current_message_id = 0;
+const salt = bcrypt.genSaltSync(5);
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.emit('your_ip', socket.handshake.address);
+    socket.emit('your_ip', bcrypt.hashSync(socket.handshake.address, salt));
 
     socket.on('message', (message) => {
-        let address = socket.handshake.address;
+        let address = bcrypt.hashSync(socket.handshake.address, salt);
 
         console.log('message: ' + message + ' from ' + address);
         socket.broadcast.emit('message', message);
